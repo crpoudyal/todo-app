c73061a8100884c64f12834d0c198de21b8f3036
chudaraj poudyal <crpoudyal@gmail.com>
2020-10-12 07:46:58 +0545

    adding buttons and div

--- js/main.js
+++ js/main.js
@@ -7,6 +7,29 @@ todoButton.addEventListener('click',addTodo);
 
 
 function addTodo(event){
-    
+    //prevent form from submitting
     event.preventDefault();
+    //todo div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add("todo");
+    //create  (li)
+    const newTodo = document.createElement('li');
+    newTodo.innerText = 'hey';
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    //check mark button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class= "fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    //check trash button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class= "fas fa-trash"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(trashButton);
+    //append to list
+    todoList.appendChild(todoDiv);
+
+
+
 }
